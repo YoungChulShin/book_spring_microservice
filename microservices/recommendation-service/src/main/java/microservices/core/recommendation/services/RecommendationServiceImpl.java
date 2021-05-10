ad4fd3ba6d8e2e64b7d3b6a8be268bbdf1a0df23
YoungChulShin <go1323@gmail.com>
2021-05-10 23:22:39 +0900

    RecommendationServiceImpl 구현

---
+++ microservices/recommendation-service/src/main/java/microservices/core/recommendation/services/RecommendationServiceImpl.java
@@ -0,0 +1,45 @@
+package microservices.core.recommendation.services;
+
+import api.core.recommendation.Recommendation;
+import api.core.recommendation.RecommendationService;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+import util.exceptions.InvalidInputException;
+import util.http.ServiceUtil;
+
+@RestController
+@RequiredArgsConstructor
+public class RecommendationServiceImpl implements RecommendationService {
+
+  private static final Logger LOG = LoggerFactory.getLogger(RecommendationServiceImpl.class);
+
+  private final ServiceUtil serviceUtil;
+
+  @Override
+  public List<Recommendation> getRecommendations(int productId) {
+    if (productId < 1) {
+      throw new InvalidInputException("Invalid productId: " + productId);
+    }
+
+    if (productId == 113) {
+      LOG.debug("No recommendations found for productId: {}", productId);
+      return new ArrayList<>();
+    }
+
+    List<Recommendation> list = new ArrayList<>();
+    list.add(new Recommendation(productId, 1, "Author 1", 1, "Content 1",
+        serviceUtil.getServiceAddress()));
+    list.add(new Recommendation(productId, 2, "Author 2", 2, "Content 2",
+        serviceUtil.getServiceAddress()));
+    list.add(new Recommendation(productId, 3, "Author 3", 3, "Content 3",
+        serviceUtil.getServiceAddress()));
+
+    LOG.debug("/recommendation response size: {}", list.size());
+
+    return list;
+  }
+}
