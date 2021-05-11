609c6ae8591bf8578c464f7b6058527583496acb
YoungChulShin <go1323@gmail.com>
2021-05-12 00:11:37 +0900

    Product service 테스트 코드 추가

--- microservices/product-service/src/main/java/microservices/core/product/services/ProductServiceImpl.java
+++ microservices/product-service/src/main/java/microservices/core/product/services/ProductServiceImpl.java
@@ -4,6 +4,7 @@ import api.core.product.Product;
 import api.core.product.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
+import util.exceptions.NotFoundException;
 import util.http.ServiceUtil;
 
 @RestController
@@ -14,6 +15,10 @@ public class ProductServiceImpl implements ProductService {
 
   @Override
   public Product getProduct(int productId) {
+
+    if (productId == 13) {
+      throw new NotFoundException("No product found for productId: " + productId);
+    }
     return new Product(productId, "name-" + productId, 123, serviceUtil.getServiceAddress());
   }
 }
